a811fa9ac4e800a2e4fe24cd922630770fa5c0f6
Krauzy <Krauzy@users.noreply.github.com>
2020-02-18 21:08:46 -0300

    Global Host

---
+++ GlobalHost/GlobalHost/API/Animation.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Runtime.InteropServices;
+
+namespace GlobalHost.API
+{
+    class Animation
+    {
+        public const int HORIZONTAL_POSITIVO = 0X1;
+        public const int HORIZONTAL_NEGATIVO = 0X2;
+        public const int VERTICAL_POSITIVO = 0X4;
+        public const int VERTICAL_NEGATIVO = 0X8;
+        public const int CENTRO = 0X10;
+        public const int BLEND = 0X80000;
+
+        [DllImport("user32.dll", CharSet = CharSet.Auto)]
+        public static extern int AnimateWindow(IntPtr ponteiro, int tempo, int flag);
+        // WinAPI.AnimateWindow(this.Handle, 500, Animation.BLEND);
+    }
+}
---
+++ GlobalHost/GlobalHost/API/Validate.cs
@@ -0,0 +1,97 @@
+﻿using System;
+
+namespace GlobalHost.API
+{
+    class Validate
+    {
+        public static bool CPF(string value)
+        {
+            value = value.Trim().Replace(".", "").Replace("-", "").Replace("/", "");
+            if (value.Length == 11)
+            {
+                int[] cpf = new int[11];
+                int soma = 0;
+                for (int i = 0; i < value.Length - 2; i++)
+                {
+                    cpf[i] = Convert.ToInt32(value[i]) - 48;
+                    soma += cpf[i] * (10 - i);
+                }
+                int resto = soma % 11;
+                int aux = 11 - resto;
+
+                if (aux > 9)
+                    cpf[9] = 0;
+                else
+                    cpf[9] = aux;
+
+                soma = 0;
+                for (int i = 0; i < value.Length - 1; i++)
+                {
+                    cpf[i] = Convert.ToInt32(value[i]) - 48;
+                    soma += cpf[i] * (11 - i);
+                }
+                resto = soma % 11;
+                aux = 11 - resto;
+
+                if (aux > 9)
+                    cpf[10] = 0;
+                else
+                    cpf[10] = aux;
+
+                for (int i = 0; i < value.Length; i++)
+                {
+                    if (cpf[i] != (Convert.ToInt32(value[i]) - 48))
+                    {
+                        return false;
+                    }
+                }
+                return true;
+            }
+            else
+            {
+                //MessageBox.Show("CPF Inválido!", "ERROR!", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return false;
+            }
+
+        }
+
+        public static bool CNPJ(string value)
+        {
+            int[] multiplicador1 = new int[12] { 5, 4, 3, 2, 9, 8, 7, 6, 5, 4, 3, 2 };
+            int[] multiplicador2 = new int[13] { 6, 5, 4, 3, 2, 9, 8, 7, 6, 5, 4, 3, 2 };
+
+            value = value.Trim().Replace(".", "").Replace("-", "").Replace("/", "");
+            if (value.Length != 14)
+                return false;
+
+            string tempCnpj = value.Substring(0, 12);
+            int soma = 0;
+
+            for (int i = 0; i < 12; i++)
+                soma += int.Parse(tempCnpj[i].ToString()) * multiplicador1[i];
+
+            int resto = (soma % 11);
+            if (resto < 2)
+                resto = 0;
+            else
+                resto = 11 - resto;
+
+            string digito = resto.ToString();
+            tempCnpj += digito;
+            soma = 0;
+            for (int i = 0; i < 13; i++)
+                soma += int.Parse(tempCnpj[i].ToString()) * multiplicador2[i];
+
+            resto = (soma % 11);
+            if (resto < 2)
+                resto = 0;
+            else
+                resto = 11 - resto;
+
+            digito += resto.ToString();
+
+            return value.EndsWith(digito);
+
+        }
+    }
+}
