10d69a0271813d9f84d15190a2c8acca927023c3
Krauzy <caiomarin26@gmail.com>
2020-05-10 20:14:46 -0300

    version 0.4.1

---
+++ GlobalHost/GlobalHost/Persistencia/CargaDB.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GlobalHost.Persistencia
+{
+    class CargaDB
+    {
+        private readonly Banco banco;
+
+        public CargaDB () {
+            this.banco = new Banco();
+        }
+
+        public bool insert()
+    }
+}
