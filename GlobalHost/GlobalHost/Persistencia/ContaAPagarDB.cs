b3f5808386dd9ab63af27b414bba5c04b3fd8199
gbrsol <gabriel.n7899@hotmail.com>
2020-05-25 19:13:30 -0300

    version 0.5.1
    Control CAP e Despesa feitos

---
+++ GlobalHost/GlobalHost/Persistencia/ContaAPagarDB.cs
@@ -0,0 +1,129 @@
+﻿using GlobalHost.Modelo;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GlobalHost.Persistencia
+{
+    class ContaAPagarDB
+    {
+        private readonly Banco banco;
+
+        public ContaAPagarDB()
+        {
+            this.banco = new Banco();
+        }
+
+        public bool Insert(object obj)
+        {
+            bool result = false;
+            if (obj.GetType() == typeof(ContaAPagar))
+            {
+                ContaAPagar c = (ContaAPagar)obj;
+                string SQL = @"INSERT INTO Contas_Pagar (valor, tipo, situacao, frete, despesa) VALUES (@valor, @tipo, @situacao, @frete, @despesa)";
+                banco.Connect();
+                result = banco.ExecuteNonQuery(SQL, "@valor", c.Valor, "@tipo", c.Tipo, "@situacao", c.Situacao, "@frete", c.Frete, "@despesa", c.Despesa);
+                banco.Disconnect();
+            }
+            return result;
+        }
+
+        public bool Delete(int id)
+        {
+            string SQL = @"DELETE FROM Contas_Pagar WHERE id = " + id;
+            banco.Connect();
+            bool result = banco.ExecuteNonQuery(SQL);
+            banco.Disconnect();
+            return result;
+        }
+
+        public bool Update(object obj)
+        {
+            bool result = false;
+            if (obj.GetType() == typeof(ContaAPagar))
+            {
+                ContaAPagar c = (ContaAPagar)obj;
+                string SQL = @"UPDATE Contas_Pagar SET valor = @valor, tipo = @tipo, situacao = @situacao, frete = @frete, despesa = @despesa";
+                banco.Connect();
+                result = banco.ExecuteNonQuery(SQL, "@valor", c.Valor, "@tipo", c.Tipo, "@situacao", c.Situacao, "@frete", c.Frete, "@despesa", c.Despesa);
+                banco.Disconnect();
+            }
+            return result;
+        }
+
+        public ContaAPagar get(int id)
+        {
+            DataTable dt = new DataTable();
+            ContaAPagar c = null;
+            string SQL = @"SELECT * FROM Contas_Pagar WHERE id = " + id;
+            banco.Connect();
+            banco.ExecuteQuery(SQL, out dt);
+            if (dt.Rows.Count > 0)
+            {
+                c = new ContaAPagar((int)dt.Rows[0]["id"],
+                                Convert.ToDouble(dt.Rows[0]["valor"]),
+                                dt.Rows[0]["tipo"].ToString(),
+                                dt.Rows[0]["situacao"].ToString(),
+                                Convert.ToInt32(dt.Rows[0]["frete"]), 
+                                Convert.ToInt32(dt.Rows[0]["despesa"]));
+            }
+            banco.Disconnect();
+            return c;
+        }
+
+        public List<object> get(string op)
+        {
+            DataTable dt = new DataTable();
+            List<object> lista = new List<object>();
+            ContaAPagar c = null;
+            string SQL = @"SELECT * FROM Contas_Pagar WHERE id = " + op;
+            banco.Connect();
+            banco.ExecuteQuery(SQL, out dt);
+            if (dt.Rows.Count > 0)
+            {
+                for (int i = 0; i < dt.Rows.Count; i++)
+                {
+                    c = new ContaAPagar((int)dt.Rows[0]["id"],
+                                Convert.ToDouble(dt.Rows[0]["valor"]),
+                                dt.Rows[0]["tipo"].ToString(),
+                                dt.Rows[0]["situacao"].ToString(),
+                                Convert.ToInt32(dt.Rows[0]["frete"]),
+                                Convert.ToInt32(dt.Rows[0]["despesa"]));
+                    lista.Add(c);
+                }
+
+            }
+            banco.Disconnect();
+            return lista;
+        }
+
+        public List<object> getAll()
+        {
+            DataTable dt = new DataTable();
+            List<object> lista = new List<object>();
+            ContaAPagar c = null;
+            string SQL = @"SELECT * FROM Contas_Pagar ORDER BY id";
+            banco.Connect();
+            banco.ExecuteQuery(SQL, out dt);
+            if (dt.Rows.Count > 0)
+            {
+                for (int i = 0; i < dt.Rows.Count; i++)
+                {
+                    c = new ContaAPagar((int)dt.Rows[0]["id"],
+                                Convert.ToDouble(dt.Rows[0]["valor"]),
+                                dt.Rows[0]["tipo"].ToString(),
+                                dt.Rows[0]["situacao"].ToString(),
+                                Convert.ToInt32(dt.Rows[0]["frete"]),
+                                Convert.ToInt32(dt.Rows[0]["despesa"]));
+                    lista.Add(c);
+                }
+
+            }
+            banco.Disconnect();
+            return lista;
+        }
+    }
+}
---
+++ GlobalHost/GlobalHost/Persistencia/DespesaDB.cs
@@ -0,0 +1,151 @@
+﻿using GlobalHost.Modelo;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GlobalHost.Persistencia
+{
+    class DespesaDB
+    {
+        private readonly Banco banco;
+
+        public DespesaDB()
+        {
+            this.banco = new Banco();
+        }
+
+        public bool Insert(object obj)
+        {
+            bool result = false;
+            if (obj.GetType() == typeof(Despesa))
+            {
+                Despesa d = (Despesa)obj;
+                string SQL = @"INSERT INTO Despesa (descricao, tipo, estado) VALUES (@descricao, @tipo, @estado)";
+                banco.Connect();
+                result = banco.ExecuteNonQuery(SQL, "descricao",d.Descricao,"@tipo",d.Tipo,"@estado",d.Estado);
+                banco.Disconnect();
+            }
+            return result;
+        }
+        public bool Insert(object obj, double valor, int parcelas)
+        {
+            bool result = false;
+            if (obj.GetType() == typeof(Despesa))
+            {
+                Despesa d = (Despesa)obj;
+                ContaAPagarDB cdb = new ContaAPagarDB();
+                string SQL = @"INSERT INTO Despesa (descricao, tipo, estado) VALUES (@descricao, @tipo, @estado)";
+                double restante = 0;
+                if (valor % parcelas == 0)
+                    valor = valor / parcelas;
+                else
+                {
+                    restante = valor % parcelas;
+                    valor = valor / parcelas;
+                }
+                banco.Connect();
+                result = banco.ExecuteNonQuery(SQL, "descricao", d.Descricao, "@tipo", d.Tipo, "@estado", d.Estado);
+                for(int i =0; i < parcelas; i++)
+                {
+                    result = cdb.Insert(new ContaAPagar(valor, d.Tipo, "PENDENTE", null, d.Id));
+                }
+                if(restante == 0)
+                    result = cdb.Insert(new ContaAPagar(restante, d.Tipo, "PENDENTE", null, d.Id));
+                banco.Disconnect();
+            }
+            return result;
+        }
+
+        public bool Delete(int id)
+        {
+            string SQL = @"DELETE FROM Despesa WHERE id = " + id;
+            banco.Connect();
+            bool result = banco.ExecuteNonQuery(SQL);
+            banco.Disconnect();
+            return result;
+        }
+
+        public bool Update(object obj)
+        {
+            bool result = false;
+            if (obj.GetType() == typeof(Despesa))
+            {
+                Despesa d = (Despesa)obj;
+                string SQL = @"UPDATE Despesa SET descricao = @descricao, tipo = @tipo, estado = @estado";
+                banco.Connect();
+                result = banco.ExecuteNonQuery(SQL, "@descricao", d.Descricao, "@tipo", d.Tipo, "@estado", d.Estado);
+                banco.Disconnect();
+            }
+            return result;
+        }
+
+        public Despesa get(int id)
+        {
+            DataTable dt = new DataTable();
+            Despesa d = null;
+            string SQL = @"SELECT * FROM Contas_Pagar WHERE id = " + id;
+            banco.Connect();
+            banco.ExecuteQuery(SQL, out dt);
+            if (dt.Rows.Count > 0)
+            {
+                d = new Despesa((int)dt.Rows[0]["id"],
+                                dt.Rows[0]["descricao"].ToString(),
+                                dt.Rows[0]["tipo"].ToString(),
+                                dt.Rows[0]["estado"].ToString());
+            }
+            banco.Disconnect();
+            return d;
+        }
+
+        public List<object> get(string op)
+        {
+            DataTable dt = new DataTable();
+            List<object> lista = new List<object>();
+            Despesa d = null;
+            string SQL = @"SELECT * FROM Despesa WHERE id = " + op;
+            banco.Connect();
+            banco.ExecuteQuery(SQL, out dt);
+            if (dt.Rows.Count > 0)
+            {
+                for (int i = 0; i < dt.Rows.Count; i++)
+                {
+                    d = new Despesa((int)dt.Rows[0]["id"],
+                                dt.Rows[0]["descricao"].ToString(),
+                                dt.Rows[0]["tipo"].ToString(),
+                                dt.Rows[0]["estado"].ToString());
+                    lista.Add(d);
+                }
+
+            }
+            banco.Disconnect();
+            return lista;
+        }
+
+        public List<object> getAll()
+        {
+            DataTable dt = new DataTable();
+            List<object> lista = new List<object>();
+            Despesa d = null;
+            string SQL = @"SELECT * FROM Despesa ORDER BY id";
+            banco.Connect();
+            banco.ExecuteQuery(SQL, out dt);
+            if (dt.Rows.Count > 0)
+            {
+                for (int i = 0; i < dt.Rows.Count; i++)
+                {
+                    d = new Despesa((int)dt.Rows[0]["id"],
+                                dt.Rows[0]["descricao"].ToString(),
+                                dt.Rows[0]["tipo"].ToString(),
+                                dt.Rows[0]["estado"].ToString());
+                    lista.Add(d);
+                }
+
+            }
+            banco.Disconnect();
+            return lista;
+        }
+    }
+}
