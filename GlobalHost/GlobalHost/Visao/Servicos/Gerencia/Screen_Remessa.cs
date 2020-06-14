d775c1587f718cf66b8fd2f18ee0bdd84a2942e3
Gabriel Campos Lopes <48593958+gcamposl@users.noreply.github.com>
2020-06-13 21:16:04 -0300

    0.5.4f
    back end remessa OK

---
+++ GlobalHost/GlobalHost/Visao/Servicos/Gerencia/Screen_Remessa.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace GlobalHost.Visao.Servicos.Gerencia
+{
+    public partial class Screen_Remessa : UserControl
+    {
+        public Screen_Remessa()
+        {
+            InitializeComponent();
+        }
+
+        private void btnInserir_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnAlterar_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnExcluir_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnOk_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnCancelar_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btMais_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btMenos_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btCEPOrigem_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btCEPDestino_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
